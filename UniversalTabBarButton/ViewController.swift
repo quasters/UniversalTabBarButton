2d090cd09f2106a0fac901dafc2c963f7b291e68
nail.burkeev <nail.burkeev@quantron-systems.com>
2023-01-19 00:32:01 +0500

    Initial Commit

---
+++ UniversalTabBarButton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversalTabBarButton
+//
+//  Created by Наиль Буркеев on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
